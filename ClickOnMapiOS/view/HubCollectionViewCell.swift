5e756ce7fef3073671af2463a642a9ed11a26343
Zoárd Geöcze <zoardag@gmail.com>
2018-09-02 17:11:20 -0300

    The Beginning

---
+++ ClickOnMapiOS/view/HubCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HubCollectionViewCell.swift
+//  ClickOnMapiOS
+//
+//  Created by Zoárd Geöcze on 20/06/2018.
+//  Copyright © 2018 Zoárd Geöcze. All rights reserved.
+//
+
+import UIKit
+
+class HubCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+}
