0d02afa190cbc5c1c210d1c8e62b885f165939bf
Zoárd Geöcze <zoardgeocze@Mac-mini-de-DPI.local>
2018-09-04 13:44:23 -0300

    First Commit from Mac Mini DPI

---
+++ ClickOnMapiOS/System Menu/Controller/SystemMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SystemMenuViewController.swift
+//  ClickOnMapiOS
+//
+//  Created by Zoárd Geöcze on 04/09/18.
+//  Copyright © 2018 Zoárd Geöcze. All rights reserved.
+//
+
+import UIKit
+
+class SystemMenuViewController : UIViewController {
+    
+    //MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
